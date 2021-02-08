f7a9e00d81764e1bbc3d402b12833ebe8992c95d
paoloviselli <46694996+paoloviselli@users.noreply.github.com>
2021-02-08 17:07:09 +0000

    imported styledComponenets (#2)
    Co-authored-by: paoloviselli <paoloviselli@192.168.1.128>

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components/native'
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Wrapper><Title>Hello World</Title></Wrapper>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -19,3 +21,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const Title = styled.Text`
+  font-size: 20px;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const Wrapper = styled.View`
+  padding: 4px;
+  background: papayawhip;
+`;
