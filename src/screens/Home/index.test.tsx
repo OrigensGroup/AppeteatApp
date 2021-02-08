9cfc0b89e7e0c8d9dacc6cc7427b6c0402f0d907
zRelux <you@example.com>
2021-02-08 18:27:21 +0000

    Add testing library

---
+++ src/screens/Home/index.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import Home from './';
+
+it('displays the example text', () => {
+  const { getByText } = render(<Home />);
+
+  expect(getByText('Example')).toBeTruthy();
+});
