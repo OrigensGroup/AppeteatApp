2b911f21073eeae19d9252409e9310a9bb36a7b6
Alessandro <hkdj@hotmail.it>
2021-02-10 18:08:57 +0100

    bottom-nav-bar finished

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,16 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { ThemeProvider } from "styled-components/native";
+import Icon from "react-native-vector-icons/Ionicons";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import Antdesign from "react-native-vector-icons/AntDesign";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import theme from "./theme";
+import { NavigationLabel } from "./styles";
 
 import Home from "./screens/Home";
+import { StyleSheet, Text } from "react-native";
+import { color } from "react-native-reanimated";
 
 const Tab = createBottomTabNavigator();
 
@@ -12,12 +19,74 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="Home" component={Home} />
-          {/* <Tab.Screen name='Locations' component={} />
-        <Tab.Screen name='Menu' component={} />
-        <Tab.Screen name='Cart' component={} />
-        <Tab.Screen name='Profile' component={} /> */}
+        <Tab.Navigator
+          initialRouteName="Home"
+          tabBarOptions={{
+            style: { height: 55 },
+            activeTintColor: "#cf9822",
+            inactiveTintColor: "#2c2c2b",
+            labelStyle: { fontSize: 10, marginBottom: 6 },
+          }}
+        >
+          <Tab.Screen
+            name="Home"
+            component={Home}
+            options={{
+              tabBarLabel: "Home",
+              tabBarIcon: ({ color, size }) => (
+                <Ionicons name="md-home-outline" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Book"
+            component={Home}
+            options={{
+              tabBarLabel: "Book",
+              tabBarIcon: ({ color, size }) => (
+                <Antdesign name="calendar" color={color} size={size} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Menu"
+            component={Home}
+            options={{
+              tabBarLabel: "Menu",
+              tabBarIcon: ({ color, size }) => (
+                <MaterialCommunityIcons
+                  name="glass-cocktail"
+                  color={color}
+                  size={size}
+                />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Cart"
+            component={Home}
+            options={{
+              tabBarLabel: "Cart",
+              tabBarIcon: ({ color, size }) => (
+                <Antdesign name="shoppingcart" color={color} size={size} />
+              ),
+            }}
+          />
+
+          <Tab.Screen
+            name="Account"
+            component={Home}
+            options={{
+              tabBarLabel: "Account",
+              tabBarIcon: ({ color, size }) => (
+                <MaterialCommunityIcons
+                  name="account-outline"
+                  color={color}
+                  size={size}
+                />
+              ),
+            }}
+          />
         </Tab.Navigator>
       </NavigationContainer>
     </ThemeProvider>
---
+++ src/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components/native";
+
+export const NavigationLabel = styled.Text`
+  color: black;
+  text-align: center;
+  font-size: 12px;
+  margin-bottom: 4px;
+`;
