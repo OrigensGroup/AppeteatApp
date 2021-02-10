d564e3ef6c2f976c259a5ca5eab30eda45e81aec
alessandrocarpanzano <hkdj@hotmail.it>
2021-02-10 18:08:28 +0100

    timer finished (#7)
    Co-authored-by: Leonardo Drici <23509971+zRelux@users.noreply.github.com>

---
+++ src/components/Homepage/Promotion/index.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { Text } from "react-native";
+import LinearGradient from "react-native-linear-gradient";
+import CountDown from "react-native-countdown-component";
+import {
+  PromotionContainer,
+  PromotionTitle,
+  styles,
+  PromotionDesc,
+  TimerWrap,
+} from "./styles";
+import { TouchableHighlight } from "react-native-gesture-handler";
+
+interface PromotionProps {}
+
+const Promotion: React.FunctionComponent<PromotionProps> = ({}) => {
+  const endDate = new Date("Wed Feb 10 2021 15:36:55 GMT+0000").getTime();
+  const dateDifference = endDate - new Date().getTime();
+  const secUntilDate = dateDifference / 1000;
+  return (
+    <PromotionContainer>
+      <LinearGradient
+        colors={["#DEB98E", "#FFB803"]}
+        style={styles.linearGradient}
+      >
+        <PromotionTitle>Happy Hour</PromotionTitle>
+        {/* <PromotionDesc>Pay 1, Get 2</PromotionDesc> */}
+        <TimerWrap>
+          <CountDown
+            until={secUntilDate}
+            onFinish={() => alert("finished")}
+            onPress={() => alert("hello")}
+            digitStyle={{ backgroundColor: "#FFF", width: 104, height: 72 }}
+            digitTxtStyle={{ color: "#000000", fontSize: 40 }}
+            timeLabelStyle={{ color: "#000000", fontSize: 15 }}
+            timeToShow={["H", "M"]}
+            size={20}
+          />
+        </TimerWrap>
+      </LinearGradient>
+    </PromotionContainer>
+  );
+};
+
+export default Promotion;
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Text } from "react-native";
+import Promotion from "../../components/Homepage/Promotion";
 import Card from "../../components/Homepage/Card";
 import Login from "../../components/Login";
 import { HomepageContainer } from "./styles";
@@ -10,6 +11,7 @@ const Home: React.FunctionComponent<HomeProps> = () => {
   return (
     <HomepageContainer>
       <Login></Login>
+      <Promotion></Promotion>
       <Card></Card>
     </HomepageContainer>
   );
