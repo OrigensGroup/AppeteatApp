96a50d9a35478f08ee7fe2c15ab55bf3fced6a4e
zRelux <leonard.drici@gmail.com>
2021-03-05 19:35:43 +0000

    Fix menu issue

--- src/screens/Menu/MenuList/styles.ts
+++ src/screens/Menu/MenuList/styles.ts
@@ -5,16 +5,7 @@ export const MenuWrapper = styled.View`
   width: 100%;
 `;
 
-export const TopContainer = styled.View`
-  margin: 0 ${({ theme }) => theme.spacing.multiple(3)};
-  margin-top: ${({ theme }) => theme.spacing.multiple(3)};
-  margin-bottom: ${({ theme }) => theme.spacing.single};
-`;
-
-export const TopBarWrapper = styled.View`
-  display: flex;
-  justify-content: space-between;
-  flex-direction: row;
-  align-items: center;
-  margin: ${({ theme }) => theme.spacing.multiple(2)} 0;
+export const SafeAreaViewBottom = styled.SafeAreaView`
+  flex: 1;
+  background: ${({ theme }) => theme.colors.textSecondary};
 `;
