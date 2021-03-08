1f39606d02e37afdd7e3c857b39ba512263fb1e4
alessandrocarpanzano <hkdj@hotmail.it>
2021-03-08 13:52:14 +0100

    Google login (#30)
    * library installation
    
    * google sign in, needs fixes
    
    * google login fixed
    
    * Fix IOS google login
    
    Co-authored-by: zRelux <leonard.drici@gmail.com>

--- src/components/Shared/GoogleButton/index.tsx
+++ src/components/Shared/GoogleButton/index.tsx
@@ -1,17 +1,36 @@
 import React from 'react';
-import { LoginManager, AccessToken } from 'react-native-fbsdk';
 import auth from '@react-native-firebase/auth';
-import {  ButtonContainer, GoogleImage } from './styles';
+import { ButtonContainer, GoogleImage } from './styles';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import { useNavigation } from '@react-navigation/core';
+
+GoogleSignin.configure({
+  webClientId: '462697324514-ukkeh7qnmf3gvc14e0klftv2h5onquf2.apps.googleusercontent.com',
+});
 
 interface GoogleButtonProps {
-   onClick?: () => void;
+  onClick?: () => void;
 }
 
 const GoogleButton: React.FunctionComponent<GoogleButtonProps> = ({ onClick }) => {
+  const navigation = useNavigation();
+  const login = () => {
+    navigation.navigate('App');
+  };
 
+  async function onGoogleButtonPress() {
+    // Get the users ID token
+    const { idToken } = await GoogleSignin.signIn();
+
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(googleCredential);
+  }
 
   return (
-    <ButtonContainer >
+    <ButtonContainer onPress={() => onGoogleButtonPress().then(() => login())}>
       <GoogleImage source={require('./../../../img/google.png')} />
     </ButtonContainer>
   );
