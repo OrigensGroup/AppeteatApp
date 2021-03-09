16682c74ba2be052e424765a4d6d167dd6c07034
zRelux <leonard.drici@gmail.com>
2021-03-09 22:33:52 +0000

    Fix horizontal padding

--- src/components/Home/Carousel/styles.ts
+++ src/components/Home/Carousel/styles.ts
@@ -4,5 +4,5 @@ export const CarouselContainer = styled.View`
   width: 100%;
 `;
 export const Container = styled.View`
-  paddinghorizontal: 15px;
+  padding-horizontal: ${({ theme }) => theme.spacing.double};
 `;
