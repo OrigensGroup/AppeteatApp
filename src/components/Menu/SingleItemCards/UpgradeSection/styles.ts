b9b20a37c4bf6679f7bb41e29330d11403a8c160
zRelux <leonard.drici@gmail.com>
2021-03-11 19:40:06 +0000

    Fix stuff

--- src/components/Menu/SingleItemCards/UpgradeSection/styles.ts
+++ src/components/Menu/SingleItemCards/UpgradeSection/styles.ts
@@ -8,6 +8,18 @@ export const UpgradeItemList = styled.SectionList`
   flex: 1;
 `;
 
+export const HeaderRow = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  margin: 0 ${({ theme }) => theme.spacing.triple};
+  padding: ${({ theme }) => theme.spacing.double} 0;
+  border-bottom-width: 1px;
+  border-color: ${({ theme }) => theme.colors.textPrimary};
+  background-color: ${({ theme }) => theme.colors.backgroundColor};
+`;
+
 export const ItemRow = styled.View`
   display: flex;
   flex-direction: row;
@@ -17,6 +29,10 @@ export const ItemRow = styled.View`
   padding: ${({ theme }) => theme.spacing.double} 0;
   border-bottom-width: 1px;
   border-color: ${({ theme }) => theme.colors.textPrimary};
+`;
+
+export const CheckBoxItemSection = styled.View`
+  margin: 0 ${({ theme }) => theme.spacing.single};
 `;
 
 export const TitleItem = styled.View`
