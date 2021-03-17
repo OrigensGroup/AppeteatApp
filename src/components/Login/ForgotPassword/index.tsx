b612c47f83e7fe5ec40bc1dca565ea7f1459f28a
zRelux <leonard.drici@gmail.com>
2021-03-17 21:57:28 +0000

    Reset password

--- src/components/Login/ForgotPassword/index.tsx
+++ src/components/Login/ForgotPassword/index.tsx
@@ -1,9 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Platform, Alert } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
-interface ForgotPasswordProps {}
+import { useTheme } from 'styled-components';
 
-const ForgotPassword: React.FunctionComponent<ForgotPasswordProps> = () => {
-  return <h1>Example</h1>;
+import auth from '@react-native-firebase/auth';
+import crashlytics from '@react-native-firebase/crashlytics';
+
+import LogInTextField from '../LogInInputField';
+import loginTranslations from '../../../translations/login';
+
+import Text from '../../shared/Text';
+import LogInButton from '../Buttons/LogInButton';
+
+import { ManualLogInContainer, TextFieldsWrapper, ButtonsWrapper, styles, GoBack } from './styles';
+
+interface ManualLogInProps {
+  changeModule: (b: 'login' | 'register' | 'forgotPassword') => void;
+}
+
+const ForgotPassword: React.FunctionComponent<ManualLogInProps> = ({ changeModule }) => {
+  const theme = useTheme();
+
+  const [email, setEmail] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const login = () => {
+    changeModule('login');
+  };
+
+  const singIn = () => {
+    crashlytics().log('Password reset attempt.');
+    setLoading(true);
+
+    auth()
+      .sendPasswordResetEmail(email)
+      .then(() => {
+        setLoading(false);
+        login();
+      })
+      .catch((error) => {
+        setLoading(false);
+
+        if (error.code === 'auth/invalid-email') {
+          Alert.alert(loginTranslations.errorSignInEmail.label);
+          return;
+        }
+
+        crashlytics().log('Password reset failed.');
+        crashlytics().recordError(error);
+        console.error(error);
+      });
+  };
+
+  return (
+    <LinearGradient
+      colors={[theme.colors.active, theme.colors.secondaryActive]}
+      end={{ x: 1, y: 1 }}
+      start={{ x: 0, y: 0 }}
+      style={styles.linearGradient}
+    >
+      <ManualLogInContainer>
+        <TextFieldsWrapper behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+          <LogInTextField
+            label={loginTranslations.forgotPassword.label}
+            placeholder={loginTranslations.forgotPassword.placeholder}
+            textContentType="emailAddress"
+            updateValue={setEmail}
+          />
+        </TextFieldsWrapper>
+        <ButtonsWrapper>
+          <GoBack onPress={login}>
+            <Text color="secondary" fontSize={14}>
+              Go Back
+            </Text>
+          </GoBack>
+          <LogInButton loading={loading} onClick={singIn} text="Reset password" />
+        </ButtonsWrapper>
+      </ManualLogInContainer>
+    </LinearGradient>
+  );
 };
 
 export default ForgotPassword;
