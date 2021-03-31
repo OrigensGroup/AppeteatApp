71a25591b7f3fb85244841ce2ce18b9b2e6c190c
paoloviselli <viselli.paolo@outlook.com>
2021-03-31 01:43:48 +0100

    readapted locatioin page to center card when only one clients only have one locations

--- src/screens/Book/LocationsList/styles.ts
+++ src/screens/Book/LocationsList/styles.ts
@@ -8,3 +8,13 @@ export const MapImage = styled.Image`
   height: 100%;
   width: 100%;
 `;
+
+export const SingleVenueContainer = styled.View`
+  display: flex;
+  align-items: center;
+  position: absolute;
+  bottom: ${({ theme }) => theme.spacing.triple};
+  left: 0px;
+  right: 0px;
+  padding: ${({ theme }) => theme.spacing.half}; 0;
+`;
