8081ad848c2007921c2f14893cd44db509f68a94
zRelux <leonard.drici@gmail.com>
2021-04-18 19:57:04 +0100

    Test app clip

---
+++ ios/OrigensTestingClip/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  OrigensTestingClip
+//
+//  Created by Leonardo Drici on 18/04/2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ ios/OrigensTestingClip/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  OrigensTestingClip
+//
+//  Created by Leonardo Drici on 18/04/2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
