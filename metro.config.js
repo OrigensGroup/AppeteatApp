a0dd250be67aef3d536ef6713aa9a47d85a1882b
zRelux <leonard.drici@gmail.com>
2021-04-21 18:15:37 +0100

    App clip progress

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,6 @@
-module.exports = {
-  transformer: {
-    assetPlugins: ['expo-asset/tools/hashAssetFiles'],
-  },
-};
+const { getDefaultConfig } = require('@expo/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+defaultConfig.transformer.assetPlugins = ['expo-asset/tools/hashAssetFiles'];
+
+module.exports = defaultConfig;
--- src/AppClip.tsx
+++ src/AppClip.tsx
@@ -1,13 +1,50 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+
+import { Bar } from './types/Bar';
+
+import CartProvider from './contexts/Cart';
+import BarProvider from './contexts/Bar';
+import UserProvider from './contexts/User';
+
+import { barInit } from './utils/loadBar';
+import { CustomTheme } from './components/shared/CustomTheme';
+import { loadStuff } from './utils/loadApp';
+import AppNavigation from './components/shared/AppNavigation';
 
 interface AppClipProps {}
 
 const AppClip: React.FunctionComponent<AppClipProps> = () => {
+  const [appReady, setAppReady] = useState(false);
+
+  const [bar, setBar] = useState<Bar>(barInit);
+
+  const initApp = async () => {
+    const foundBar = await loadStuff();
+    setBar(foundBar);
+  };
+
+  useEffect(() => {
+    const load = async () => {
+      await initApp();
+    };
+
+    load().then(() => setAppReady(true));
+  }, []);
+
+  if (!appReady) {
+    return <></>;
+  }
+
   return (
-    <View>
-      <Text>Example</Text>
-    </View>
+    <BarProvider loadedBar={bar}>
+      <UserProvider>
+        <CartProvider>
+          <CustomTheme>
+            <AppNavigation />
+          </CustomTheme>
+        </CartProvider>
+      </UserProvider>
+    </BarProvider>
   );
 };
 
