09ea2311fff68a8f980c2e57879eb0aea56efa23
zRelux <leonard.drici@gmail.com>
2021-04-21 14:47:45 +0100

    Fix origens testing build

--- android/app/src/main/java/com/appfe/generated/BasePackageList.java
+++ android/app/src/main/java/com/appfe/generated/BasePackageList.java
@@ -14,13 +14,9 @@ public class BasePackageList {
         new expo.modules.filesystem.FileSystemPackage(),
         new expo.modules.font.FontLoaderPackage(),
         new expo.modules.imageloader.ImageLoaderPackage(),
-        new expo.modules.keepawake.KeepAwakePackage(),
-        new expo.modules.lineargradient.LinearGradientPackage(),
-        new expo.modules.location.LocationPackage(),
         new expo.modules.notifications.NotificationsPackage(),
         new expo.modules.permissions.PermissionsPackage(),
         new expo.modules.securestore.SecureStorePackage(),
-        new expo.modules.sqlite.SQLitePackage(),
         new expo.modules.updates.UpdatesPackage()
     );
   }
